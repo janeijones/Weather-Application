68edf6ebd9cedff080db1f9bd79354aafe7c39cf
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-01 02:09:13 -0500

    Updated header styles and html. Converting containers to cards for easier manipulation

--- Assets/blank.js
+++ Assets/blank.js
@@ -1,3 +1,9 @@
+var searchBtnEl = document.querySelector("#search-button");
+var cityListEl = document.querySelector("#city-list");
+var cityInput = document.querySelector('#city-text');
+var citySearchForm = document.querySelector('#city-input');
+var cities = [];
+
 var geocodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q=London,us&limit=2&appid=edcce0198e0ff90af79e7f1e4745aedf';
 var wUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.8865&lon=-83.4483&exclude=hourly,minutely&units=imperial&appid=edcce0198e0ff90af79e7f1e4745aedf';
 var key = 'edcce0198e0ff90af79e7f1e4745aedf' 
@@ -12,6 +18,69 @@ var fiveDayForecastEl = document.querySelector('#five-day-forecast')
 var fiveDayTitleEl = document.querySelector('#five-day-title')
 
 
+function renderCityList(){
+
+    cityListEl.innerHTML = "";
+    
+    for (var i=0; i<cities.length; i++) {
+        var city = cities[i];
+        var li = document.createElement("li");
+        li.textContent = city;
+        li.classList.add("list-group-item");
+        li.setAttribute("data-index", i);
+        
+        cityListEl.appendChild(li);
+    }
+}
+
+function storeCityList() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+
+function loadCityList() {  //function retrieves city list from local storage, runs on page load
+    var storedCities = JSON.parse(localStorage.getItem("cities"))
+
+    if (storedCities !== null) { // if data is located in local storage, update cities array 
+        cities = storedCities;
+    }
+
+    renderCityList(); //calls renderCityList function to load cities array on page
+}
+
+searchBtnEl.addEventListener("click", function(event) {
+    var cityText = cityInput.value.trim();
+
+    if (cityText === ""){
+        return;
+    }
+
+    cities.push(cityText);
+    cityInput.value = "";
+
+    storeCityList();
+    renderCityList();
+    
+});
+
+citySearchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var cityText = cityInput.value.trim();
+
+    if (cityText === ""){
+        return;
+    }
+
+    cities.push(cityText);
+    cityInput.value = "";
+
+    storeCityList();
+    renderCityList();
+
+    var cityApi = openWeatherApi + "q=" + cityText + "&units=imperial" + apiKey; 
+    console.log(cityApi);
+    getCityApi(cityApi);   
+});
 
 
 function getGeoData (geoApi) {
@@ -98,6 +167,8 @@ function getWeather (lat, lon) {
                         {
                         var futureDayEl = document.createElement('div');
                         futureDayEl.classList.add('future-forecast')
+                        futureDayEl.classList.add('card')
+                        futureDayEl.classList.add('col-md-2')
                         var futureDate = moment().add([i], 'days').format('MM/DD/YY');
                         var futureTemp = parseInt(data.daily[i].temp.max);
                         var futureHumidity = parseInt(data.daily[i].humidity)           
